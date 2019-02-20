34170a6b9b2e44a9dd8d8b56b7f77cab0d00a83a
Alexander <xitman78@gmail.com>
2019-02-21 00:15:12 +0200

    deploy fix

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,28 +8,26 @@ import { AboutPageQuery } from "../graphql-types"
 const PageTitle = styled.h1`
 `
 
-class AboutPage extends React.Component<{data: AboutPageQuery, location: any}> {
-  render() {
-    const siteTitle = this.props.data!.site!.siteMetadata!.title!
+const AboutPage = ({data, location}: {data: AboutPageQuery, location: any}) =>  {
+  const siteTitle = data!.site!.siteMetadata!.title!
 
-    return (
-      <Layout location={location} title={siteTitle}>
-      <SEO title="About Me"/>
-        <PageTitle>About Me</PageTitle>
-        <p>
-          Welcome to my blog. My name is Alexander and I'm merely a human being. At least I believe in that.
-          That's all I can say about myself at the moment.
-        </p>
-        <p>
-          If you want to know more find me on
-          &nbsp;<a href="https://www.instagram.com/alexander_cherepnya/">Instagram</a>,&nbsp;
-          <a href="https://www.facebook.com/alexander.cherepnya">Facebook</a>,&nbsp;
-          <a href="https://www.linkedin.com/in/alexander-cherepnya-1149a632/">LinkedIn</a> and&nbsp;
-          <a href="https://github.com/xitman78">GitHub</a>
-        </p>
-      </Layout>
-    )
-  }
+  return (
+    <Layout location={location} title={siteTitle}>
+    <SEO title="About Me"/>
+      <PageTitle>About Me</PageTitle>
+      <p>
+        Welcome to my blog. My name is Alexander and I'm merely a human being. At least I believe in that.
+        That's all I can say about myself at the moment.
+      </p>
+      <p>
+        If you want to know more find me on
+        &nbsp;<a href="https://www.instagram.com/alexander_cherepnya/">Instagram</a>,&nbsp;
+        <a href="https://www.facebook.com/alexander.cherepnya">Facebook</a>,&nbsp;
+        <a href="https://www.linkedin.com/in/alexander-cherepnya-1149a632/">LinkedIn</a> and&nbsp;
+        <a href="https://github.com/xitman78">GitHub</a>
+      </p>
+    </Layout>
+  )
 }
 
 export default AboutPage
